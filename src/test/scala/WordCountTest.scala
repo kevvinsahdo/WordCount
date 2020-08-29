726b4b201c787c39725e4dd16e21e11f595c3057
Kevvin Sahdo <kevvinsd@gmail.com>
2020-08-29 16:21:30 -0400

    test: Create test to getwords method

---
+++ src/test/scala/WordCountTest.scala
@@ -0,0 +1,27 @@
+package com.wordcount
+
+import org.scalatest.funsuite.AnyFunSuite
+import org.apache.spark.sql.{DataFrame, SparkSession}
+
+
+class WordCountTest extends AnyFunSuite {
+  val sparkSession: SparkSession = {
+    SparkSession
+      .builder()
+      .master("local")
+      .appName("WordCountTest")
+      .getOrCreate()
+  }
+  import sparkSession.implicits._
+
+  test("Test getTweetWords remove stop words and clean tweets") {
+    val tweetDataframe = Seq(("#test123"), ("I"), ("test")).toDF("text")
+    val stopWordsDataframe = Seq(("you"), ("i"), ("yourself")).toDF("word")
+
+    val tweetWords = WordCount.getTweetsWords(tweetDataframe, stopWordsDataframe)
+
+    assert((tweetWords.count() == 2))
+    assert((tweetWords.first.getString(0) === "test123"))
+    assert(tweetWords.isInstanceOf[DataFrame])
+  }
+}
